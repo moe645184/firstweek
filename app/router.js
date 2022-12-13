dd80fa9919868f1dd5e85fec231df7a60805c0ab
moe645184 <108288076+moe645184@users.noreply.github.com>
2022-12-13 10:29:31 +0800

    coding_style

--- app/router.js
+++ app/router.js
@@ -7,5 +7,5 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   router.resources('message', '/message', controller.messages);
-  router.post("/edit/:id",controller.messages.renew);
+  router.post('/edit/:id',controller.messages.renew);
 };
